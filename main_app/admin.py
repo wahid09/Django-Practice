4d232de654a108e4dd69fc4fa0298393361cd35e
wahid09 <wahid.ullah.cse@gmail.com>
2020-02-10 22:27:11 +0600

    add peoduct model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from .models import PersonelInfo
+from .models import Product
 
 # Register your models here.
+class PersonelInfoAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'appoinment', 'joining_date']
+    list_display = ['name', 'appoinment', 'joining_date']
+    list_editable = ['appoinment', 'joining_date']
+    list_filter = ['joining_date']
+    list_per_page = 10
+    class Meta:
+        model = PersonelInfo
 
-admin.site.register(PersonelInfo)
+
+admin.site.register(PersonelInfo, PersonelInfoAdmin)
+
+# Product Model
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'description', 'price']
+    list_display = ['title', 'price', 'active', 'updated']
+    list_editable = ['price', 'active']
+    list_filter = ['price', 'active']
+    readonly_fields = ['updated', 'timestamp']
+    prepopulated_fields = {"slug": ("title",)}
+    list_per_page = 10
+
+    class Meta:
+        module = Product
+
+admin.site.register(Product, ProductAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,14 @@ class PersonelInfo(models.Model):
     appoinment = models.CharField(max_length=30)
     joining_date = models.DateField()
 
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=100, default=29.99)
+    slug = models.SlugField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.title
